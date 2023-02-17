4c0d016b22f7441ac0ad9ed3205517011e16f985
sumidaca <sumidaca@hawaii.edu>
2023-02-17 02:44:53 -1000

    Tried adding Mongo

---
+++ PassPass/ClientApp/src/components/MongoTest.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+export class MongoTest extends Component {
+  static displayName = MongoTest.name;
+
+  constructor(props) {
+    super(props);
+    this.state = { books: [], loading: true };
+  }
+
+  componentDidMount() {
+    this.getBookData();
+  }
+
+  static renderBooksTable(books) {
+    return (
+      <table className='table table-striped' aria-labelledby="tabelLabel">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Category</th>
+            <th>Author</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map(book =>
+            <tr key={book.id}>
+              <td>{book.bookName}</td>
+              <td>{book.price}</td>
+              <td>{book.category}</td>
+              <td>{book.author}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    );
+  }
+
+  render() {
+    let contents = this.state.loading
+      ? <p><em>Loading...</em></p>
+      : MongoTest.renderBooksTable(this.state.books);
+
+    return (
+      <div>
+        <h1 id="tabelLabel" >Books</h1>
+        <p>This component demonstrates fetching book data from the server.</p>
+        {contents}
+      </div>
+    );
+  }
+
+  async getBookData() {
+    const response = await fetch('Books');
+    const data = await response.json();
+    this.setState({ forecasts: data, loading: false });
+  }
+}
--- PassPass/Program.cs
+++ PassPass/Program.cs
@@ -1,6 +1,17 @@
+using BookStoreApi.Models;
+using BookStoreApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+
+builder.Services.AddSingleton<BooksService>();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(
+        options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 
 builder.Services.AddControllersWithViews();
 
