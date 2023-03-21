ccb83c376ab53bed8f8d5d2a9d752caf3fee617f
kerensaa <kerensaa@stud.ntnu.no>
2023-03-20 22:31:39 -1000

    create empty login page

---
+++ PassPass/ClientApp/src/components/Login.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export class Login extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  
+
+
+  render() {
+    return (
+      <div>
+        <h1>Login Page</h1>
+      </div>
+
+    );
+  }
+}
