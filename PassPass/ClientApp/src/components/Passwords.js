918edf16e3f74aabd2047025bfb26773bb0e1b29
lisemnilsen <lmnilsen@hawaii.edu>
2023-04-10 21:17:17 -1000

    Update password page #5

--- PassPass/ClientApp/src/components/Passwords.js
+++ PassPass/ClientApp/src/components/Passwords.js
@@ -1,42 +1,16 @@
 import React, { Component } from 'react';
 
 
-export class MongoTest extends Component {
-  static displayName = MongoTest.name;
+export class Passwords extends Component {
+  static displayName = Passwords.name;
   
   constructor(props) {
     super(props);
-    this.state = { books: [], passwords: [], loading: true, loading2: true };
+    this.state = { passwords: [], loading2: true };
   }
 
   componentDidMount() {
-    this.getBookData();
     this.getPasswordData();
-  }
-
-  static renderBooksTable(books) {
-    return (
-      <table className='table table-striped' aria-labelledby="tabelLabel">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Category</th>
-            <th>Author</th>
-          </tr>
-        </thead>
-        <tbody>
-          {books.map(book =>
-            <tr key={book.id}>
-              <td>{book.Name}</td>
-              <td>{book.price}</td>
-              <td>{book.category}</td>
-              <td>{book.author}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    );
   }
 
   static renderPasswordsTable(passwords) {
@@ -44,7 +18,7 @@ export class MongoTest extends Component {
       <table className='table table-striped' aria-labelledby="tabelLabe2">
         <thead>
           <tr>
-            <tr>App User</tr>
+            <th>App User</th>
             <th>Username</th>
             <th>Website</th>
             <th>Password</th>
@@ -70,7 +44,7 @@ export class MongoTest extends Component {
     const website = event.target.website.value;
     const username = event.target.username.value;
     const password = event.target.password.value;
-    console.log(`Website: ${website}, Username: ${username}, Password: ${password}`);
+    console.log(`Website: ${website}, Username: ${username} , Password: ${password}`);
   
     const response = await fetch('/api/passwords', {
       method: 'POST',
@@ -89,43 +63,28 @@ export class MongoTest extends Component {
   };
 
   render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : MongoTest.renderBooksTable(this.state.books);
-
     let contents2 = this.state.loading2
       ? <p><em>Loading...</em></p>
-      : MongoTest.renderPasswordsTable(this.state.passwords);  
+      : Passwords.renderPasswordsTable(this.state.passwords);  
 
     return (
       <div>
-        <h1 id="tabelLabel">Books</h1>
-        <p>This component demonstrates fetching book data from the server.</p>
-        {contents}
         <h1 id="tabe2Label">Passwords</h1>
-        <p>This component demonstrates a mockup of the password table.</p>
+        <p>An overview of the stored passwords.</p>
         {contents2}
-        
- 
+
+        <div className='mt-5'/>
+
         <form id="myForm" onSubmit={this.handleSubmit}>
-  Website: <input type="text" name="website"  required></input>
-  Website Username: <input type="text" name="username" required></input>
-  Password: <input type="text" id="password" required></input>
-  <button type="submit" onclick="DesperateTest()" >Submit</button>
-  </form> 
+          Website: <input type="text" name="website" style={{marginRight: '20px'}}  required></input>
+          Website Username: <input type="text" name="username" style={{marginRight: '20px'}} required></input>
+          Password: <input type="text" id="password" required></input>
+          <button type="submit" onclick="DesperateTest()" >Submit</button>
+        </form> 
       </div>
-       
     );
   }
 
-  async getBookData() {
-    const response = await fetch('api/books');
-    console.log(response);
-    const data = await response.json();
-    console.log(data);
-    //console.log(Environment.GetEnvironmentVariable("MONGO_URL"));
-    this.setState({ books: data, loading: false });
-  }
   async getPasswordData() {
     const response = await fetch('api/passwords');
     console.log(response);
@@ -135,4 +94,3 @@ export class MongoTest extends Component {
     this.setState({ passwords: data, loading2: false });
   }
 }
-
