6d6c197aba04bdce1276f6c3e86b073296481cc9
kerensaa <kerensaa@stud.ntnu.no>
2023-04-10 21:37:51 -1000

    Update About.js

--- PassPass/ClientApp/src/components/About.js
+++ PassPass/ClientApp/src/components/About.js
@@ -19,7 +19,14 @@ export class About extends Component {
     return (
       <div>
         <h1 className='display-3 mt-5'> About PassPass: </h1>
-        <p>This is a....</p>
+        <p><br></br> Protect your online accounts with our password manager! <br></br>
+        With the increasing number of online services and the growing threat of data breaches, 
+        having strong and unique passwords for each account has become essential.<br></br>
+        Our password manager offers a reliable and convenient solution to generate, 
+        store, and manage complex passwords, so you don't have to remember them all.<br></br> <br></br>
+        Join us in creating a safer online environment for everyone.
+
+        </p>
         {/* <h1>Counter</h1>
 
         <p>This is a simple example of a React component.</p>
